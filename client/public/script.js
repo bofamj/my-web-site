563f1536c82f808eccd584f0c4153186d07bda5d
unknown <mustafamjaber@yahoo.com>
2022-08-23 15:09:18 +0200

    add the about me section animation

--- client/public/script.js
+++ client/public/script.js
@@ -1,8 +1,33 @@
+/* navbar inmation */
 gsap.from("#title", {duration: 1, y: -100});
 gsap.from(".nav-text-1", {duration: 1.5, y: -100});
 gsap.from(".nav-text-2", {duration: 2, y: -100});
 gsap.from(".nav-text-3", {duration: 2.5, y: -100});
 
+
+const containers = gsap.utils.toArray(".fade");
+containers.forEach((container) => {
+    let tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: container,
+        pin: true,
+        pinSpacing: false,
+        scrub: true,
+        markers: false
+      }
+    });
+  
+    tl.from(container, {
+      autoAlpha: 1
+    }).to(
+      container,
+      {
+        autoAlpha: 0
+      },
+      3.5
+    );
+  });
+/* gsap.from("#about", {duration:2.5, opacity:0, stagger:0.05, ease:"power2.in"}) */
 
 
 /* hero image */
