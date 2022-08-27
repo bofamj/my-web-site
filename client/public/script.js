0b67688d3c3ee59466db3b5e7553b3ccb960d28f
unknown <mustafamjaber@yahoo.com>
2022-08-27 13:35:19 +0200

    add the animation for all sections

--- client/public/script.js
+++ client/public/script.js
@@ -1,45 +1,146 @@
-/* navbar inmation */
-gsap.from("#title", {duration: 1, y: -100});
-gsap.from(".nav-text-1", {duration: 1.5, y: -100});
-gsap.from(".nav-text-2", {duration: 2, y: -100});
-gsap.from(".nav-text-3", {duration: 2.5, y: -100});
+//* navbar inmation */
+gsap.from(".nav-text", {
+  duration: 2,
+  x: -100,
+  ease: "bounce.out",
+});
+gsap.from("#title", { duration: 2, y: -100, ease: "bounce.out" });
 
+//* hero image */
+gsap.from(".image", { duration: 2, x: 1000, ease: "back.out(1.7)" });
 
-const containers = gsap.utils.toArray(".fade");
-containers.forEach((container) => {
-    let tl = gsap.timeline({
-      scrollTrigger: {
-        trigger: container,
-        pin: true,
-        pinSpacing: false,
-        scrub: true,
-        markers: false
-      }
-    });
-  
-    tl.from(container, {
-      autoAlpha: 1
-    }).to(
-      container,
-      {
-        autoAlpha: 0
-      },
-      3.5
-    );
-  });
-/* gsap.from("#about", {duration:2.5, opacity:0, stagger:0.05, ease:"power2.in"}) */
+//* the hero text animation */
+gsap.from(".hero-text", {
+  duration: 2,
+  stagger: {
+    each: 0.1,
+    from: "center",
+  },
+  x: -1000,
+});
 
+//* about me section */
+gsap.from(".about-me", {
+  scrollTrigger: {
+    trigger: ".about-me",
+    toggleActions: "restart none restart noen",
+  }, //* start the animation when ".box" enters the viewport (once)
+  x: -500,
+  scrub: 1,
+});
+gsap.from(".about-mee", {
+  scrollTrigger: {
+    trigger: ".about-mee",
+    toggleActions: "restart none restart noen",
+  }, //* start the animation when ".box" enters the viewport (once)
+  x: 1050,
+  duration: 1,
+});
 
-/* hero image */
-gsap.from(".image", {duration: 1, x: 1000});
+//* progect section */
+gsap.from(".section-project", {
+  scrollTrigger: {
+    trigger: ".section-project",
+    toggleActions: "restart none restart noen",
+  },
+  duration: 2,
+  y: -200,
+  ease: "bounce.out",
+});
 
+gsap.from(".project-1-desc", {
+  scrollTrigger: {
+    trigger: ".project-1-desc",
+    toggleActions: "restart none restart noen",
+  }, //* start the animation when ".box" enters the viewport (once)
+  x: -500,
+  scrub: 1,
+});
+gsap.from(".project-1-img", {
+  scrollTrigger: {
+    trigger: ".project-1-img",
+    toggleActions: "restart none restart noen",
+  }, //* start the animation when ".box" enters the viewport (once)
+  x: 1050,
+  duration: 1,
+});
+gsap.from(".project-2-desc", {
+  scrollTrigger: {
+    trigger: ".project-2-desc",
+    toggleActions: "restart none restart noen",
+  }, //* start the animation when ".box" enters the viewport (once)
+  x: 1050,
+  scrub: 1,
+});
+gsap.from(".project-2-img", {
+  scrollTrigger: {
+    trigger: ".project-2-img",
+    toggleActions: "restart none restart noen",
+  }, //* start the animation when ".box" enters the viewport (once)
+  x: -500,
+  duration: 1,
+});
+gsap.from(".project-3-desc", {
+  scrollTrigger: {
+    trigger: ".project-3-desc",
+    toggleActions: "restart none restart noen",
+  }, //* start the animation when ".box" enters the viewport (once)
+  x: -500,
+  scrub: 1,
+});
+gsap.from(".project-3-img", {
+  scrollTrigger: {
+    trigger: ".project-3-img",
+    toggleActions: "restart none restart noen",
+  }, //* start the animation when ".box" enters the viewport (once)
+  x: 1050,
+  duration: 1,
+});
 
+//*contact mr section*/
+gsap.from(".section-contact", {
+  scrollTrigger: {
+    trigger: ".section-contact",
+    toggleActions: "restart none restart noen",
+  },
+  duration: 2,
+  y: -200,
+  ease: "bounce.out",
+});
+gsap.from(".email", {
+  scrollTrigger: {
+    trigger: ".email",
+    toggleActions: "restart none restart noen",
+  }, //* start the animation when ".box" enters the viewport (once)
+  x: -500,
+  scrub: 1,
+});
+gsap.from(".text", {
+  scrollTrigger: {
+    trigger: ".text",
+    toggleActions: "restart none restart noen",
+  }, //* start the animation when ".box" enters the viewport (once)
+  x: 1050,
+  duration: 1,
+});
+//* sklls  section*/
+gsap.from(".section-skills", {
+  scrollTrigger: {
+    trigger: ".section-skills",
+    toggleActions: "restart none restart noen",
+  },
+  duration: 2,
+  y: -200,
+  ease: "bounce.out",
+});
 
-
-/* the hero text animation */
-gsap.from(".hero-text-1", {duration: 1, x: -1000});
-gsap.from(".hero-text-2", {duration: 1.5, x: -1000});
-gsap.from(".hero-text-3", {duration: 2, x: -1000});
-gsap.from(".hero-text-4", {duration: 2.5, x: -1000});
-gsap.from(".hero-text-5", {duration: 3, x: -1000});
-gsap.from(".hero-text-6", {duration: 3.5, x: -1000});
+gsap
+  .from(".skills-icons", {
+    scrollTrigger: {
+      trigger: ".skills-icons",
+      toggleActions: "restart none restart noen",
+    },
+    opacity: 0,
+    duration: 3,
+  })
+  .to(".skills-icons", { opacity: 1 });
