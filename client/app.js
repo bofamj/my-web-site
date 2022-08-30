20c7e219af4d02a54f62e202978b458bd00a24e1
unknown <mustafamjaber@yahoo.com>
2022-08-30 10:37:36 +0200

    add the send email functionalty

--- client/app.js
+++ client/app.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const nodemailer = require("nodemailer");
@@ -19,8 +19,7 @@ const mile = process.env.RECIPIENT;
 const user = process.env.GMAIL_USER;
 const OAuth2 = google.auth.OAuth2;
 
-
-//*nodemailer refresh token
+///*nodemailer refresh token
 const Refresh_Token =
   "1//04aA8QTWDCUB9CgYIARAAGAQSNwF-L9IrYxiCX7rbZE0MDRGiXVv4q3DDDOseElzciUWCMkZx4B0j7X_rBElHOpBJSERm0vLF5rA";
 
@@ -31,7 +30,6 @@ const oauth2Client = new OAuth2(
   "https://developers.google.com/oauthplayground"
 );
 
-
 //*oauth2 credentials
 oauth2Client.setCredentials({ refresh_token: Refresh_Token });
 const accessToken = oauth2Client.getAccessToken();
@@ -39,9 +37,7 @@ const accessToken = oauth2Client.getAccessToken();
 //*sending the main html page rout
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
-  console.log(id,secret,mile,user)
 });
-
 
 const data = {
   host: "smtp.gmail.com",
@@ -49,20 +45,17 @@ const data = {
   secure: true,
   auth: {
     type: "OAuth2",
-    user:"enserali79@gmail.com" ,
-    clientId:id,
-    clientSecret: secret,
+    user: "enserali79@gmail.com",
+    clientId:
+      "754540055745-dk295k74uv341ntkmfp7lk2llsu0d6pj.apps.googleusercontent.com",
+    clientSecret: "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",
     refreshToken: Refresh_Token,
     accessToken: accessToken,
   },
-}
+};
 
 //*sending the email route
 app.post("/", (req, response) => {
-  console.log(req.body.email, req.body.message);
-  /* console.log(data); */
-  
-
   const output = `
     <p>You have a new contact request</p>
     <h3>Contact details</h3>
@@ -72,18 +65,18 @@ app.post("/", (req, response) => {
   const smtpTrans = nodemailer.createTransport(data);
 
   const mailOpts = {
-    from:"enserali79@gmail.com" ,
-    to:"mustafamjaber@yahoo.com" ,
+    from: req.body.email,
+    to: "mustafamjaber@yahoo.com",
     subject: "New message from Nodemailer-contact-form",
     html: output,
   };
-  console.log(req.body.email);
+
   smtpTrans.sendMail(mailOpts, (error, res) => {
     if (error) {
       console.log(error);
     } else {
-      console.log(user, id, secret, mile);
-      response.status(200).send("Successfully ");
+      /* console.log(user, id, secret, mile); */
+      response.status(200);
     }
   });
 });
--- client/public/script.js
+++ client/public/script.js
@@ -1,9 +1,22 @@
 let mybutton = document.getElementById("btn-back-to-top");
 
-// When the user scrolls down 20px from the top of the document, show the button
+//* When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function () {
   scrollFunction();
 };
+
+//*  handell the contact button */
+const contectBtn = document.getElementById("subBtn");
+console.log(contectBtn);
+contectBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  //* @desc emti the input filed after submiting the form */
+  let maile = e.target.parentNode.childNodes[1].childNodes[3];
+  maile.value = "";
+  let text = e.target.parentNode.childNodes[3].childNodes[3];
+  text.value = "";
+});
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
@@ -12,7 +25,7 @@ function scrollFunction() {
     mybutton.style.display = "none";
   }
 }
-// When the user clicks on the button, scroll to the top of the document
+//* When the user clicks on the button, scroll to the top of the document
 mybutton.addEventListener("click", backToTop);
 
 function backToTop() {
